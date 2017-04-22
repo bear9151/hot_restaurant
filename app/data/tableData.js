223dc1084dea8a1cc14066eb215a0d9e434f91e9
Barrett <barrett.allen@icloud.com>
2017-04-22 13:04:08 -0400

    Pushing working JSONs and Routes

---
+++ app/data/tableData.js
@@ -0,0 +1,20 @@
+var tables = [{
+   routeName: "russell",
+   name: "Russell",
+   partySize: 2,
+   time: "5pm"
+
+}, {
+   routeName: "allen",
+   name: "Allen",
+   partySize: 5,
+   time: "7pm"
+
+}, {
+   routeName: "kong",
+   name: "Kong",
+   partySize: 1,
+   time: "3pm"
+}];
+
+module.exports = tables;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,8 @@
 // These data sources hold arrays of information on table-data & waitinglist
 // ===============================================================================
 
-
+var tables = require("../data/tableData.js"),
+	waitinglist = require("../data/waitinglistData.js");
 
 
 // ===============================================================================
@@ -14,5 +15,35 @@
 module.exports = function(app) {
 
 	//add get and post routes for api
+	app.get("/app/:customer?", function(req, res) {
+		var chosen = req.params.customer;
+
+		if (chosen) {
+			for (var i = 0; i < tables.length; i++) {
+				if (chosen === tables[i].routeName) {
+					return res.json(tables[i]);
+				}
+			}
+			return res.json(false);
+		}
+		return res.json(tables);
+	})
+
+	app.get("/waitinglist/:customer?", function(req, res) {
+		var chosen = req.params.customer;
+
+		if (chosen) {
+			console.log(chosen);
+
+			for (var i = 0; i < waitinglist.length; i++) {
+				if (chosen === waitinglist[i].routeName) {
+					return res.json(waitinglist[i]);
+				}
+			}
+			return res.json(false);
+		}
+		return res.json(waitinglist);
+	})
+
 
 };
